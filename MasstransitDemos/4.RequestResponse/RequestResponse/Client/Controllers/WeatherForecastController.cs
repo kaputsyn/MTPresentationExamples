5212247b79dff08991bf3a79c40c9c6cd919402c
OFFICEKIEV\a.koposov <a.koposov@temabit.com>
2021-08-31 15:26:14 +0300

    RequestResponse

---
+++ MasstransitDemos/4.RequestResponse/RequestResponse/Client/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IRequestClient<IGetWeather> _requestClient;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IRequestClient<IGetWeather> requestClient)
+    {
+        _logger = logger;
+        _requestClient = requestClient;
+    }
+
+    [HttpGet]
+    [Produces(typeof(IWeatherResponse))]
+    public async Task<IActionResult> Get(string location)
+    {
+        var response = await _requestClient.GetResponse<IWeatherNotAwailable, IWeatherResponse>(new
+        {
+            Location = location
+        });
+
+        if (response.Is<IWeatherNotAwailable>(out var notAwailable)) 
+        {
+            return NotFound(notAwailable.Message.Reason);
+        }
+        if (response.Is<IWeatherResponse>(out var weather))
+        {
+            return Ok(weather.Message);
+        }
+        else 
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ MasstransitDemos/4.RequestResponse/RequestResponse/Contracts/IWeatherResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts
+{
+    public interface IWeatherResponse
+    {
+        double HumidityPercent { get; }
+        double TemperatureKelvin { get; }
+
+        DateTime TimeStamp { get; }
+
+        string Location { get; }
+
+    }
+}

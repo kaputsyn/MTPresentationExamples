93342a9a65f1f312686968d190e14fbcd20c0d97
OFFICEKIEV\a.koposov <a.koposov@temabit.com>
2021-09-03 15:20:28 +0300

    Saga

---
+++ MasstransitDemos/5.Saga/Saga/Saga.Contracts/OrderSubmitted.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Saga.Contracts
+{
+    public interface OrderSubmitted
+    {
+        Guid OrderId { get; }
+        DateTime TimeStamp { get; }
+
+        string CustomerNumber { get; }
+    }
+}
